2c8f0c2f79abf83e5943e28efcd92c5c2ce3bb43
akashmore83386 <akashmore83386@gmail.com>
2022-11-04 01:01:34 +0530

    changes in second branch index

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 const show = (firstname) =>{
-    console.log(`Hi ${firstname}, this is the git and github crash course..............`)
+    console.log(`Hi ${firstname}, this is the git and github crash dog..............`)
 }
 const firstname = 'akash';
 show(firstname)
