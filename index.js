3eeccf7bc7728e21e98b9edd5b90c84b38248ba8
akashmore83386 <akashmore83386@gmail.com>
2022-11-03 22:29:41 +0530

    done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 const show = (firstname) =>{
-    console.log(`Hi ${firstname}, this is the git crash course`)
+    console.log(`Hi ${firstname}, this is the git and github crash course`)
 }
 const firstname = 'akash';
 show(firstname)
