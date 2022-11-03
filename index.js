1272bcebad7c120dddfd7a687287c008d65cc967
akashmore83386 <akashmore83386@gmail.com>
2022-11-04 01:08:21 +0530

    some changes

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 const show = (firstname) =>{
     console.log(`Hi ${firstname}, this is the git and github crash dog..............`)
 }
-const firstname = 'akash';
+const firstname = 'akash'
 show(firstname)
